2d4dfcfca0525497b28732c4492e75cb39498ed2
rowdark <rowdark1993@163.com>
2016-05-11 02:01:54 +0800

    compress DelaunayTriangulation.cpp

--- improve/DelaunayTriangulation.cpp
+++ improve/DelaunayTriangulation.cpp
@@ -7,82 +7,48 @@ Triangulation::add_point 将某点加入三角剖分
 某个 Triangle 在三角剖分中当且仅当它的 has_children 为 0
 如果要找到三角形 u 的邻域, 则枚举它的所有 u.edge[i].tri, 该条边的两个点为 u.p[(i+1)%3], u.p[(i+2)%3]
 */
-const int N = 100000 + 5;
-const int MAX_TRIS = N * 6;
-const double EPSILON = 1e-6;
-const double PI = acos(-1.0);
-using namespace std;
-
+const int N = 100000 + 5, MAX_TRIS = N * 6;
+const double EPSILON = 1e-6, PI = acos(-1.0);
 struct Point {
-	double x,y;
-	Point() : x(0), y(0) {}
-	Point(double x, double y) : x(x), y(y) {}
-	inline bool operator == (Point const& that) const {
-		return x == that.x && y == that.y;
-	}
+	double x,y; Point():x(0),y(0){} Point(double x, double y):x(x),y(y){}
+	bool operator ==(Point const& that)const {return x==that.x&&y==that.y;}
 };
-
 inline double sqr(double x) { return x*x; }
-double dist_sqr(Point const& a, Point const& b) {
-	return sqr(a.x-b.x) + sqr(a.y-b.y);
-}
+double dist_sqr(Point const& a, Point const& b){return sqr(a.x-b.x)+sqr(a.y-b.y);}
 bool in_circumcircle(Point const& p1, Point const& p2, Point const& p3, Point const& p4) {
-	double u11 = p1.x - p4.x;
-	double u21 = p2.x - p4.x;
-	double u31 = p3.x - p4.x;
-	double u12 = p1.y - p4.y;
-	double u22 = p2.y - p4.y;
-	double u32 = p3.y - p4.y;
+	double u11 = p1.x - p4.x, u21 = p2.x - p4.x, u31 = p3.x - p4.x;
+	double u12 = p1.y - p4.y, u22 = p2.y - p4.y, u32 = p3.y - p4.y;
 	double u13 = sqr(p1.x) - sqr(p4.x) + sqr(p1.y) - sqr(p4.y);
 	double u23 = sqr(p2.x) - sqr(p4.x) + sqr(p2.y) - sqr(p4.y);
 	double u33 = sqr(p3.x) - sqr(p4.x) + sqr(p3.y) - sqr(p4.y);
 	double det = -u13*u22*u31 + u12*u23*u31 + u13*u21*u32 - u11*u23*u32 - u12*u21*u33 + u11*u22*u33;
 	return det > EPSILON;
 }
-double side(Point const& a, Point const& b, Point const& p) {
-	return (b.x-a.x)*(p.y-a.y) - (b.y-a.y)*(p.x-a.x);
-}
-
-typedef int SideRef;
-struct Triangle;
-typedef Triangle* TriangleRef;
+double side(Point const& a, Point const& b, Point const& p) { return (b.x-a.x)*(p.y-a.y) - (b.y-a.y)*(p.x-a.x);}
+typedef int SideRef; struct Triangle; typedef Triangle* TriangleRef;
 struct Edge {
-	TriangleRef tri;
-	SideRef     side;
-	Edge() : tri(0), side(0) {}
+	TriangleRef tri; SideRef side; Edge() : tri(0), side(0) {}
 	Edge(TriangleRef tri, SideRef side) : tri(tri), side(side) {}
 };
 struct Triangle {
-	Point p[3];
-	Edge  edge[3];
-	TriangleRef children[3];
-	Triangle() {}
+	Point p[3]; Edge edge[3]; TriangleRef children[3]; Triangle() {}
 	Triangle(Point const& p0, Point const& p1, Point const& p2) {
-		p[0] = p0; p[1] = p1; p[2] = p2;
-		children[0] = children[1] = children[2] = 0;
+		p[0]=p0;p[1]=p1;p[2]=p2;children[0]=children[1]=children[2]=0;
 	}
-	bool has_children() const {
-		return children[0] != 0;
-	}
+	bool has_children() const { return children[0] != 0; }
 	int num_children() const {
 		return children[0] == 0 ? 0
 			: children[1] == 0 ? 1
 			: children[2] == 0 ? 2 : 3;
 	}
 	bool contains(Point const& q) const {
-		double a = side(p[0],p[1],q);
-		double b = side(p[1],p[2],q);
-		double c = side(p[2],p[0],q);
+		double a=side(p[0],p[1],q), b=side(p[1],p[2],q), c=side(p[2],p[0],q);
 		return a >= -EPSILON && b >= -EPSILON && c >= -EPSILON;
 	}
 } triange_pool[MAX_TRIS], *tot_triangles;
 void set_edge(Edge a, Edge b) {
 	if (a.tri) a.tri->edge[a.side] = b;
 	if (b.tri) b.tri->edge[b.side] = a;
-	if (a.tri && b.tri) {
-		assert(a.tri->p[(a.side+1)%3] == b.tri->p[(b.side+2)%3]);
-		assert(a.tri->p[(a.side+2)%3] == b.tri->p[(b.side+1)%3]);
-	}
 }
 class Triangulation {
 	public:
@@ -90,96 +56,46 @@ class Triangulation {
 			const double LOTS = 1e6;
 			the_root = new(tot_triangles++) Triangle(Point(-LOTS,-LOTS),Point(+LOTS,-LOTS),Point(0,+LOTS));
 		}
-		~Triangulation() {}
-		TriangleRef find(Point p) const {
-			return find(the_root,p);
-		}
-		void add_point(Point const& p) {
-			add_point(find(the_root,p),p);
-		}
+		TriangleRef find(Point p) const { return find(the_root,p); }
+		void add_point(Point const& p) { add_point(find(the_root,p),p); }
 	private:
 		TriangleRef the_root;
 		static TriangleRef find(TriangleRef root, Point const& p) {
 			for( ; ; ) {
-				assert(root->contains(p));
-				if (!root->has_children()) {
-					return root;
-				} else {
-					int flag = true;
-					for (int i = 0; i < 3 && root->children[i] ; ++i) {
-						if (root->children[i]->contains(p)) {
-							root = root->children[i];
-							break;
-						}
-					}
-					assert(flag&&"point not found");
-				}
+				if (!root->has_children()) return root;
+				else for (int i = 0; i < 3 && root->children[i] ; ++i)
+						if (root->children[i]->contains(p))
+							{root = root->children[i]; break;}
 			}
 		}
 		void add_point(TriangleRef root, Point const& p) {
 			TriangleRef tab,tbc,tca;
-			/* split it into three triangles */
 			tab = new(tot_triangles++) Triangle(root->p[0], root->p[1], p);
 			tbc = new(tot_triangles++) Triangle(root->p[1], root->p[2], p);
 			tca = new(tot_triangles++) Triangle(root->p[2], root->p[0], p);
-			set_edge(Edge(tab,0), Edge(tbc,1));
-			set_edge(Edge(tbc,0), Edge(tca,1));
-			set_edge(Edge(tca,0), Edge(tab,1));
-			set_edge(Edge(tab,2), root->edge[2]);
-			set_edge(Edge(tbc,2), root->edge[0]);
-			set_edge(Edge(tca,2), root->edge[1]);
-			root->children[0] = tab;
-			root->children[1] = tbc;
-			root->children[2] = tca;
-			flip(tab,2);
-			flip(tbc,2);
-			flip(tca,2);
+			set_edge(Edge(tab,0),Edge(tbc,1));set_edge(Edge(tbc,0),Edge(tca,1));
+			set_edge(Edge(tca,0),Edge(tab,1));set_edge(Edge(tab,2),root->edge[2]);
+			set_edge(Edge(tbc,2),root->edge[0]);set_edge(Edge(tca,2),root->edge[1]);
+			root->children[0]=tab;root->children[1]=tbc;root->children[2]=tca;
+			flip(tab,2); flip(tbc,2); flip(tca,2);
 		}
 		void flip(TriangleRef tri, SideRef pi) {
-			TriangleRef trj = tri->edge[pi].tri;
-			int pj = tri->edge[pi].side;
-			if (!trj) return;
-			if (!in_circumcircle(tri->p[0],tri->p[1],tri->p[2],trj->p[pj])) return;
-			assert(tri->p[(pi+2)%3] == trj->p[(pj+1)%3]);
-			assert(tri->p[(pi+1)%3] == trj->p[(pj+2)%3]);
-			/* flip edge between tri,trj */
+			TriangleRef trj = tri->edge[pi].tri; int pj = tri->edge[pi].side;
+			if(!trj||!in_circumcircle(tri->p[0],tri->p[1],tri->p[2],trj->p[pj])) return;
 			TriangleRef trk = new(tot_triangles++) Triangle(tri->p[(pi+1)%3], trj->p[pj], tri->p[pi]);
 			TriangleRef trl = new(tot_triangles++) Triangle(trj->p[(pj+1)%3], tri->p[pi], trj->p[pj]);
 			set_edge(Edge(trk,0), Edge(trl,0));
-			set_edge(Edge(trk,1), tri->edge[(pi+2)%3]);
-			set_edge(Edge(trk,2), trj->edge[(pj+1)%3]);
-			set_edge(Edge(trl,1), trj->edge[(pj+2)%3]);
-			set_edge(Edge(trl,2), tri->edge[(pi+1)%3]);
-			tri->children[0] = trk; tri->children[1] = trl; tri->children[2] = 0;
-			trj->children[0] = trk; trj->children[1] = trl; trj->children[2] = 0;
-			flip(trk,1);
-			flip(trk,2);
-			flip(trl,1);
-			flip(trl,2);
+			set_edge(Edge(trk,1), tri->edge[(pi+2)%3]); set_edge(Edge(trk,2), trj->edge[(pj+1)%3]);
+			set_edge(Edge(trl,1), trj->edge[(pj+2)%3]); set_edge(Edge(trl,2), tri->edge[(pi+1)%3]);
+			tri->children[0]=trk;tri->children[1]=trl;tri->children[2]=0;
+			trj->children[0]=trk;trj->children[1]=trl;trj->children[2]=0;
+			flip(trk,1); flip(trk,2); flip(trl,1); flip(trl,2);
 		}
 };
-
-int n;
-Point ps[N];
-
-void build()
-{
-	tot_triangles = triange_pool;
-	cin >> n;
-	for(int i = 0; i < n; ++ i) {
-		int x, y;
-		scanf("%d%d", &x, &y);
-		ps[i].x = x; ps[i].y = y;
-	}
-	random_shuffle(ps, ps + n);
-	Triangulation tri;
-	for(int i = 0; i < n; ++ i) {
-		tri.add_point(ps[i]);
-	}
-}
-
-int main()
-{
-	build();
-	return 0;
+int n; Point ps[N];
+void build(){
+	tot_triangles = triange_pool; cin >> n;
+	for(int i = 0; i < n; ++ i) scanf("%lf%lf",&ps[i].x,&ps[i].y);
+	random_shuffle(ps, ps + n); Triangulation tri;
+	for(int i = 0; i < n; ++ i) tri.add_point(ps[i]);
 }
