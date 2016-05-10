6bb748dc36a9ca0ae85497acda632f34a417a991
Necrololicon <haobinni@163.com>
2016-05-10 22:23:12 +0800

    Added 3DGeo

---
+++ merge/3DGeo.cpp
@@ -0,0 +1,50 @@
+struct Point3D {
+	double x, y, z;
+};
+
+Point3D det(const Point3D &a, const Point3D &b) {
+	return Point3D(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
+}
+//平面法向量:平面上两个向量叉积
+//点共平面:平面上一点与之的向量点积法向量为0
+//点在线段(直线)上:共线且两边点积非正
+//点在三角形内(不包含边界，需再判断是与某条边共线)
+bool pointInTri(const Point3D &a, const Point3D &b, const Point3D &c, const Point3D &p) {
+	return sign(det(a - b, a - c).len() - det(p - a, p - b).len() - det(p - b, p - c).len() - det(p - c, p - a).len()) == 0;
+}
+//共平面的两点是否在这平面上一条直线的同侧
+bool sameSide(const Point3D &a, const Point3D &b, const Point3D &p0, const Point3D &p1) {
+	return sign(dot(det(a - b, p0 - b), det(a - b, p1 - b))) > 0;
+}
+//两点在平面同侧:点积法向量符号相同
+//两直线平行/垂直:同二维
+//平面平行/垂直:判断法向量
+//线面垂直:法向量和直线平行
+//判断空间线段是否相交:四点共面两线段不平行相互在异侧
+//线段和三角形是否相交:线段在三角形平面不同侧 三角形任意两点在线段和第三点组成的平面的不同侧
+//求空间直线交点
+Point3D intersection(const Point3D &a0, const Point3D &b0, const Point3D &a1, const Point3D &b1) {
+	double t = ((a0.x - a1.x) * (a1.y - b1.y) - (a0.y - a1.y) * (a1.x - b1.x)) / ((a0.x - b0.x) * (a1.y - b1.y) - (a0.y - b0.y) * (a1.x - b1.x));
+	return a0 + (b0 - a0) * t;
+}
+//求平面和直线的交点
+Point3D intersection(const Point3D &a, const Point3D &b, const Point3D &c, const Point3D &l0, const Point3D &l1) {
+	Point3D p = pVec(a, b, c); //平面法向量
+	double t = (p.x * (a.x - l0.x) + p.y * (a.y - l0.y) + p.z * (a.z - l0.z)) / (p.x * (l1.x - l0.x) + p.y * (l1.y - l0.y) + p.z * (l1.z - l0.z));
+	return l0 + (l1 - l0) * t;
+}
+//求平面交线:取不平行的一条直线的一个交点,以及法向量叉积得到直线方向
+//点到直线距离:叉积得到三角形的面积除以底边
+//点到平面距离:点积法向量
+//直线间距离:平行时随便取一点求距离,否则叉积方向向量得到方向点积计算长度
+//直线夹角:点积 平面夹角:法向量点积
+//三维向量旋转操作(绕向量 s 旋转 ang 角度)，对于右手系s指向观察者时逆时针
+//矩阵版:
+void rotate(const Point3D &s, double ang) {
+	double l = s.len(), x = s.x / l, y = s.y / l, z = s.z / l, sinA = sin(ang), cosA = cos(ang);
+	double p[4][4]= {CosA + (1 - CosA) * x * x, (1 - CosA) * x * y - SinA * z, (1 - CosA) * x * z + SinA * y, 0,
+		(1 - CosA) * y * x + SinA * z, CosA + (1 - CosA) * y * y, (1 - CosA) * y * z - SinA * x, 0,
+		(1 - CosA) * z * x - SinA * y, (1 - CosA) * z * y + SinA * x, CosA + (1 - CosA) * z * z, 0,
+		0, 0, 0, 1 };
+}
+//计算版:把需要旋转的向量按照 s 分解,做二维旋转,再回到三维
