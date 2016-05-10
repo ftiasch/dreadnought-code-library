2dd02d47467b8a36b1ba414d77a43cef73e17c87
Necrololicon <haobinni@163.com>
2016-05-10 23:18:35 +0800

    Added Cirlce

---
+++ merge/CircleArea.cpp
@@ -0,0 +1,78 @@
+struct Event {
+	Point p;
+	double ang;
+	int delta;
+	Event (Point p = Point(0, 0), double ang = 0, double delta = 0) : p(p), ang(ang), delta(delta) {}
+};
+
+bool operator < (const Event &a, const Event &b) {
+	return a.ang < b.ang;
+}
+
+void addEvent(const Circle &a, const Circle &b, vector<Event> &evt, int &cnt) {
+	double d2 = (a.o - b.o).len2(),
+		   dRatio = ((a.r - b.r) * (a.r + b.r) / d2 + 1) / 2,
+		   pRatio = sqrt(-(d2 - sqr(a.r - b.r)) * (d2 - sqr(a.r + b.r)) / (d2 * d2 * 4));
+	Point d = b.o - a.o, p = d.rotate(PI / 2),
+		  q0 = a.o + d * dRatio + p * pRatio,
+		  q1 = a.o + d * dRatio - p * pRatio;
+	double ang0 = (q0 - a.o).ang(),
+		   ang1 = (q1 - a.o).ang();
+	evt.push_back(Event(q1, ang1, 1));
+	evt.push_back(Event(q0, ang0, -1));
+	cnt += ang1 > ang0;
+}
+
+bool issame(const Circle &a, const Circle &b) {
+	return sign((a.o - b.o).len()) == 0 && sign(a.r - b.r) == 0;
+}
+
+bool overlap(const Circle &a, const Circle &b) {
+	return sign(a.r - b.r - (a.o - b.o).len()) >= 0;
+}
+
+bool intersect(const Circle &a, const Circle &b) {
+	return sign((a.o - b.o).len() - a.r - b.r) < 0;
+}
+
+int C;
+Circle c[N];
+double area[N]; 
+
+void solve() {
+	memset(area, 0, sizeof(double) * (C + 1));
+	for (int i = 0; i < C; ++i) {
+		int cnt = 1;
+		vector<Event> evt;
+		for (int j = 0; j < i; ++j) {
+			if (issame(c[i], c[j])) {
+				++cnt;
+			}
+		}
+		for (int j = 0; j < C; ++j) {
+			if (j != i && !issame(c[i], c[j]) && overlap(c[j], c[i])) {
+				++cnt; 
+			}
+		}
+		for (int j = 0; j < C; ++j) {
+			if (j != i && !overlap(c[j], c[i]) && !overlap(c[i], c[j]) && intersect(c[i], c[j])) {
+				addEvent(c[i], c[j], evt, cnt);
+			}
+		}
+		if (evt.size() == 0) {
+			area[cnt] += PI * c[i].r * c[i].r;
+		} else {
+			sort(evt.begin(), evt.end());
+			evt.push_back(evt.front());
+			for (int j = 0; j + 1 < (int)evt.size(); ++j) {
+				cnt += evt[j].delta;
+				area[cnt] += det(evt[j].p, evt[j + 1].p) / 2;
+				double ang = evt[j + 1].ang - evt[j].ang;
+				if (ang < 0) {
+					ang += PI * 2;
+				}
+				area[cnt] += ang * c[i].r * c[i].r / 2 - sin(ang) * c[i].r * c[i].r / 2;
+			}
+		}
+	}
+}
---
+++ merge/areaCT.cpp
@@ -0,0 +1,27 @@
+double areaCT(Point pa, Point pb, double r) {
+	if (pa.len() < pb.len()) {
+		swap(pa, pb);
+	}
+	if (sign(pb.len()) == 0) {
+		return 0;
+	}
+	double a = pb.len(), b = pa.len(), c = (pb - pa).len();
+	double sinB = fabs(det(pb, pb - pa) / a / c),
+		   cosB = dot(pb, pb - pa) / a / c, 
+		   sinC = fabs(det(pa, pb) / a/ b),
+		   cosC = dot(pa, pb) / a / b;
+	double B = atan2(sinB, cosB), C = atan2(sinC, cosC);
+	if (a > r) {
+		S = C / 2 * r * r;
+		h = a * b * sinC / c;
+		if (h < r && B < PI / 2) {
+			S -= (acos(h / r) * r * r - h * sqrt(r * r - h * h));
+		}
+	} else if (b > r) {
+		double theta = PI - B - asin(sinB / r * a);
+		S = a * r * sin(theta) / 2 + (C - theta) / 2 * r * r;
+	} else {
+		S = sinC * a * b / 2;
+	}
+	return S;
+}
