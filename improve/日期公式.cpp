1ad9c4653707f92044a8e61c4134427c078c7f58
rowdark <wenhanhuang1993@gmail.com>
2016-12-08 08:09:07 +0000

    fixed

--- improve/日期公式.cpp
+++ improve/日期公式.cpp
@@ -4,6 +4,6 @@ int zeller(int y,int m,int d) {
 	if (w<0) w+=7; return(w);
 }
 int getId(int y, int m, int d) {
-	if (m < 3) {y --; m += 12};
-	return 365 * y + y / 4 - y / 100 + y / 400 + (153 * (m - 3) + 2) / 5 + d;
+	if (m < 3) {y --; m += 12;}
+	return 365 * y + y / 4 - y / 100 + y / 400 + (153 * (m - 3) + 2) / 5 + d - 307;
 }
